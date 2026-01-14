af33887a1814e97a60c0dd650a819c1c7c8eda78
Behzad Ghabaei <Ghabaei_Behzad@student.smc.edu>
2026-01-13 19:47:49 -0800

    Add get length function

--- journal.js
+++ journal.js
@@ -1,9 +1,24 @@
-// Function 1: Greet a user by name
+//Behzad Ghabaei
+//Assignment 3A
+
+//Function 1: Greet a user by name
 function greet(name) { 
     return "Hello, " + name + "!"; 
 }
-
-function isEven(num) { return num % 2 === 0; }
-function square(num) { return num * num; }
-function celsiusToFahrenheit(c) { return (c * 9/5) + 32; }
+//Function 2: is even
+function isEven(num) { 
+	return num % 2 === 0; 
+}
+//Function 3: square
+function square(num) { 
+	return num * num; 
+}
+//Function 4: convert temp.
+function celsiusToFahrenheit(c) { 
+	return (c * 9/5) + 32; 
+}
+//Function 5: string length
+function getLength(str) { 
+	return str.length; 
+}
 
